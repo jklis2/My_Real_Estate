a779616410d5d1cca2e7901c27af66c393e2f30b
Jakub Kliś <jakubklis2201@gmail.com>
2024-03-05 13:06:26 +0100

    feat: Create carousel in Latest Offers

---
+++ Client/src/consts/slides.ts
@@ -0,0 +1,5 @@
+export const SLIDES = [
+  { location: "Krakow, PL", url: "url('../assets/Gallery1.png')" },
+  { location: "London, UK", url: "url('../assets/Gallery2.png')" },
+  { location: "Paris, FR", url: "url('../assets/Gallery3.png')" },
+];
--- Client/src/containers/LatestOffers.tsx
+++ Client/src/containers/LatestOffers.tsx
@@ -1,39 +1,59 @@
-import { useState } from 'react';
+import React from "react";
+import Slider from "react-slick";
 import H2 from "../components/H2";
 import OfferCard from "../components/OfferCard";
 import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import { SLIDES } from "../consts/slides";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+interface ArrowProps {
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+const NextArrow = ({ onClick }: ArrowProps) => {
+  return (
+    <div
+      className="text-neutral-700 absolute right-0 top-1/2 transform -translate-y-1/2 cursor-pointer z-10"
+      onClick={onClick}
+    >
+      <RiArrowRightSLine size={100} />
+    </div>
+  );
+};
+
+const PrevArrow = ({ onClick }: ArrowProps) => {
+  return (
+    <div
+      className="text-neutral-700 absolute left-0 top-1/2 transform -translate-y-1/2 cursor-pointer z-10"
+      onClick={onClick}
+    >
+      <RiArrowLeftSLine size={100} />
+    </div>
+  );
+};
 
 export default function LatestOffers() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const slides = [
-    { location: "Krakow, PL", url: "url('../assets/Gallery1.png')" },
-    { location: "London, UK", url: "url('../assets/Gallery2.png')" },
-    { location: "Paris, FR", url: "url('../assets/Gallery3.png')" }
-  ];
-
-  const prevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1);
-  };
-
-  const nextSlide = () => {
-    setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
 
   return (
-    <section className="lg:my-16 text-center">
+    <section className="lg:my-16 text-center relative">
       <H2>Latest Offers</H2>
-      <div className="flex gap-8 justify-center items-center my-10 relative lg:max-w-screen-lg mx-auto">
-        <RiArrowLeftSLine size={200} className="absolute left-0 top-1/2 transform -translate-y-1/2 cursor-pointer z-10" onClick={prevSlide} />
-        {slides.map((slide, index) => (
-          <OfferCard
-            key={index}
-            location={slide.location}
-            url={slide.url}
-            style={{ transform: `translateX(-${currentSlide * 100}%)` }}
-          />
+      <Slider {...settings}>
+        {SLIDES.map((slide, index) => (
+          <div key={index} className="px-4">
+            <OfferCard location={slide.location} url={slide.url} />
+          </div>
         ))}
-        <RiArrowRightSLine size={200} className="absolute right-0 top-1/2 transform -translate-y-1/2 cursor-pointer" onClick={nextSlide} />
-      </div>
+      </Slider>
     </section>
   );
 }
