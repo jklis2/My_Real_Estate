858b1fb2ebbbfd696fbbf0e53ff369fea7f365c7
Jakub Kliś <jakubklis2201@gmail.com>
2024-03-07 17:51:10 +0100

    feat: Add Users and Properties management for Admin

--- Client/src/routes/PropertyManagement.tsx
+++ Client/src/routes/PropertyManagement.tsx
@@ -1,3 +1,15 @@
+import H2 from "../components/H2";
+import SearchForm from "../components/SearchForm";
+import Table from "../components/Table";
+
 export default function PropertyManagement() {
-  return <div>AllProperties</div>;
+  return (
+    <div className="mx-8">
+      <H2>Propierties management</H2>
+      <SearchForm />
+      <div className="mt-6">
+        <Table />
+      </div>
+    </div>
+  );
 }
--- Client/src/routes/UsersManagement.tsx
+++ Client/src/routes/UsersManagement.tsx
@@ -1,3 +1,15 @@
+import H2 from "../components/H2";
+import SearchForm from "../components/SearchForm";
+import Table from "../components/Table";
+
 export default function UsersManagement() {
-  return <div>Users</div>;
+  return (
+    <div className="mx-8">
+      <H2>Users management</H2>
+      <SearchForm />
+      <div className="mt-6">
+      <Table />
+      </div>
+    </div>
+  );
 }
