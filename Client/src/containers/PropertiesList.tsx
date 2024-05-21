580fbd45a30fde59950d08b67fbb81b116caecc4
Jakub Kliś <jakubklis2201@gmail.com>
2024-05-21 14:18:50 +0200

    style: Add placeholder image for properties without photos in Dashboard

--- Client/src/containers/PropertiesList.tsx
+++ Client/src/containers/PropertiesList.tsx
@@ -2,6 +2,7 @@ import H2 from "../components/H2";
 import PropertyCard from "../components/PropertyCard";
 import { Address } from "../interfaces/Address";
 import { Property } from "../interfaces/Property";
+import PlaceholderImage from "../assets/PropertyPlaceholder.png";
 
 interface Addresses {
   [key: string]: {
@@ -26,18 +27,25 @@ export default function PropertiesList({
         {title} ({properties?.length?.toString()})
       </H2>
       <div className="grid gap-10 lg:grid-cols-3 md:grid-cols-2">
-        {!properties && <p>You don't have any properties yet.</p>}
+        {(!properties || properties.length === 0) && (
+          <p>You don't have any properties yet.</p>
+        )}
         {properties &&
           properties.length > 0 &&
-          properties?.map((prop) => {
+          properties.map((prop) => {
             const { city } = addresses[prop.id]?.result[0] || "";
+            const imgPath =
+              prop.photos && prop.photos.length > 0
+                ? `${import.meta.env.VITE_API_URL}Photo?propertyId=${
+                    prop.id
+                  }&photoId=${prop.photos[0]}`
+                : PlaceholderImage;
             return (
               <PropertyCard
+                key={prop.id}
                 name={prop.name}
                 location={city}
-                imgPath={`${import.meta.env.VITE_API_URL}Photo?propertyId=${
-                  prop.id
-                }&photoId=${prop.photos[0]}`}
+                imgPath={imgPath}
               />
             );
           })}
